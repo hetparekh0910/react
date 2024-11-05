e4b0e0653316127b7c00706f80d5e6d8e9c7363f
hetparekh0910 <hetparekh701@gmail.com>
2024-11-05 13:26:21 +0530

    first commit

---
+++ myapp/src/home.js
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+
+const Home =() => {
+    const [name, setName]=useState("roro")
+        const handleClick=()=> {
+            alert("hello world")
+        }
+        const changeName= ()=> {
+            setName ("xyz")
+
+        }
+        const [ todo ,setTodo]= useState([
+            {
+                (alias) useState
+                }
+            }
+        ])
+}
+ 
+
+
+
+
+const home =()=>{
+    return (
+
+    );
+
+}
